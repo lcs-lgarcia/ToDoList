898190dadc13fcbfa40103f925002f6e800e6403
Lucas Garcia <lgarciaalonso@lcs.on.ca>
2023-04-06 11:36:23 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Lucas García on 6/4/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
